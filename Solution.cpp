c9aa65a67b31c9268a06e8d9733199cac86cc12e
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-12-10 18:24:24 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,35 @@
+
+using namespace std;
+
+class Solution {
+public:
+
+    bool isStrobogrammatic(string num) {
+
+        /*
+        Alternatively, we can apply Map.
+     
+        Array 'int_rotated_180_degrees': 
+        Strobogrammatic numbers: 0=>0, 1=>1, 6=>9, 8=>8, 9=>6 
+        Non-strobogrammatic numbers: 2=>*, 3=>*, 4=>*, 5=>*, 7=>* 
+         */
+        char int_rotated_180_degrees[] = {'0', '1', '*', '*', '*', '*', '9', '*', '8', '6'};
+        int leftPointer = 0;
+        int rightPointer = num.length() - 1;
+
+        while (leftPointer <= rightPointer) {
+
+            if (int_rotated_180_degrees[num[leftPointer] - '0'] == '*') {
+                return false;
+            }
+            if (int_rotated_180_degrees[num[leftPointer] - '0'] != num[rightPointer]) {
+                return false;
+            }
+
+            leftPointer++;
+            rightPointer--;
+        }
+
+        return true;
+    }
+};
