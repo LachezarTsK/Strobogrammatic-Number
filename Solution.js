f10bb13a17176d507dd96135039052dd4c8e5c08
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-12-10 18:21:58 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,33 @@
+
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isStrobogrammatic = function (num) {
+
+    /*
+     Aletrnatively, we can apply Map.
+     
+     Array 'int_rotated_180_degrees': 
+     Strobogrammatic numbers: 0=>0, 1=>1, 6=>9, 8=>8, 9=>6 
+     Non-strobogrammatic numbers: 2=>*, 3=>*, 4=>*, 5=>*, 7=>* 
+     */
+    const int_rotated_180_degrees = ['0', '1', '*', '*', '*', '*', '9', '*', '8', '6'];
+    let leftPointer = 0;
+    let rightPointer = num.length - 1;
+
+    while (leftPointer <= rightPointer) {
+
+        if (int_rotated_180_degrees[num.charAt(leftPointer) - '0'] === '*') {
+            return false;
+        }
+        if (int_rotated_180_degrees[num.charAt(leftPointer) - '0'] !== num.charAt(rightPointer)) {
+            return false;
+        }
+
+        leftPointer++;
+        rightPointer--;
+    }
+
+    return true;
+};
